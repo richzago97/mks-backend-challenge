3b87f6139eb96f9af819d228c09d6e3cc152500d
richzago97 <rich.czago@gmail.com>
2023-11-25 11:58:49 -0300

    echo: configured modules auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LoginValidationMiddleware } from './middlewares/validation.middleware';
+import { PassportModule } from '@nestjs/passport';
+import { config } from 'dotenv';
+config();
+
+
+@Module({
+  imports: [UserModule, PassportModule, JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: {expiresIn: '12h'}
+  })],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  
+})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginValidationMiddleware).forRoutes('login');
+  }
+}
