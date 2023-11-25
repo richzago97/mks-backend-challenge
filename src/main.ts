5492d13b095c7328297ae01c4695e442a1f607b3
richzago97 <rich.czago@gmail.com>
2023-11-25 15:41:24 -0300

    feat: added swagger automatic

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api')
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Documentação API MKS Movies')
+    .setDescription(
+      '',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
