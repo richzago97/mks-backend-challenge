59da16cd1f749793f46bc3be5fe65863287a82e2
richzago97 <rich.czago@gmail.com>
2023-11-25 11:56:38 -0300

    feat: created user entity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserModel {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    password: string;
+}
